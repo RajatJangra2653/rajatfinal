27767083001e68371365740a1e1d15f01b6c15a9
Rajat Jangra <162875200+RajatJangra2653@users.noreply.github.com>
2024-04-10 23:30:09 +0530

    Update RajatFinal.ps1

--- RajatFinal.ps1
+++ RajatFinal.ps1
@@ -1,67 +1,22 @@
-$edgeInstallerUrl = "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BC11F6829-D84A-B108-1780-8BD39A096C6C%7D%26lang%3Den-GB%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe"
-$edgeInstallerPath = "$env:TEMP\ChromeSetup.exe"
-
 New-Item -ItemType File -Path "$env:TEMP\logfile.txt"
 
 $logFilePath = "$env:TEMP\logfile.txt"
 
 Start-Transcript -Path $logFilePath -Append
 
-Invoke-WebRequest -Uri $edgeInstallerUrl -OutFile $edgeInstallerPath
+# Set the URL for the Chrome installer
+$chromeInstallerUrl = "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BC11F6829-D84A-B108-1780-8BD39A096C6C%7D%26lang%3Den-GB%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe"
 
-Start-Process -FilePath $edgeInstallerPath
+# Set the path for the downloaded Chrome installer
+$chromeInstallerPath = "$env:TEMP\ChromeSetup.exe"
 
-Remove-Item $edgeInstallerPath -Force
+# Download Chrome installer
+Invoke-WebRequest -Uri $chromeInstallerUrl -OutFile $chromeInstallerPath
 
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServer
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-CommonHttpFeatures
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpErrors
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpRedirect
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationDevelopment
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-NetFxExtensibility45
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-HealthAndDiagnostics
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpLogging
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-LoggingLibraries
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestMonitor
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpTracing
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-Security
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestFiltering
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-Performance
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpCompressionStatic
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpCompressionDynamic
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerManagementTools
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-ManagementConsole
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-IIS6ManagementCompatibility
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-Metabase
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-ManagementScriptingTools
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-BasicAuthentication
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebSockets
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-StaticContent
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-DefaultDocument
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-DirectoryBrowsing
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebDAV
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIExtensions
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIFilter
+# Install Chrome silently
+Start-Process msiexec.exe -ArgumentList "/i `"$chromeInstallerPath`" /qn" -Wait
 
-Start-Service W3SVC
-
-Set-Service W3SVC -StartupType Automatic
-
-$defaultPagePath = "C:\inetpub\wwwroot\index.html"
-@"
-<!DOCTYPE html>
-<html>
-<head>
-<title>Welcome to IIS</title>
-</head>
-<body>
-<h1>Welcome to IIS Server</h1>
-<p>This is the default page of Internet Information Services (IIS) Server.</p>
-</body>
-</html>
-"@ | Out-File -FilePath $defaultPagePath -Encoding utf8
-
+# Delete the downloaded file
+Remove-Item -Path $chromeInstallerPath -Force
 # Stop transcript
 Stop-Transcript
