bdce84951f650cbe0ef4dbc84da79a414b7a2ea1
Rajat Jangra <162875200+RajatJangra2653@users.noreply.github.com>
2024-04-11 21:37:28 +0530

    Update FinalCseToRunAScript.ps1

--- FinalCseToRunAScript.ps1
+++ FinalCseToRunAScript.ps1
@@ -4,38 +4,14 @@ Start-Transcript -Path $logFilePath -Append
 $url = "https://drive.usercontent.google.com/download?id=1uUwnvqqMWRCZ2L33J2lOLXcCuQFka-Xc&export=download&authuser=0&confirm=t&uuid=051cd117-8e88-40da-8111-9fcad42c199c&at=APZUnTUzd73NR6YZ_fku52ndq_qu:1712846598660"
 $tempFilePath = "C:\Users\Public\chromeandiis.ps1"
 Invoke-WebRequest -Uri $url -OutFile $tempFilePath
-# Get the SID of the user
 
 function Get-CurrentUserSID {
     $currentUserSID = whoami /user /fo csv | ConvertFrom-Csv | Select-Object -ExpandProperty SID
     return $currentUserSID
 }
 
-# Get the SID of the current user using the function
-#$currentSID = Get-CurrentUserSID
-
-# Use the SID to fetch information about the current user
-#$currentUserInfo = Get-WmiObject -Class Win32_UserAccount -Filter "SID='$currentSID'"
-
-# Output information about the current user
-#$currentUserInfo
-
-# Define the username and password
-$username = "rajatvm/rajat"
-$password = ConvertTo-SecureString "Rjangra#2021" -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential($username, $password)
-
-# Create the scheduled task trigger
-$trigger = New-ScheduledTaskTrigger -AtLogOn
-
-# Create the scheduled task action
+$currentSID = Get-CurrentUserSID
+$trigger = New-ScheduledTaskTrigger -AtLogOn -User $currentSID
 $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File $tempFilePath"
-
-# Register the scheduled task with authentication
-Register-ScheduledTask -TaskName ABC -Action $action -Trigger $trigger -User $username -Password $password -TaskPath "\" -Credential $credential
-
-
-# $trigger = New-ScheduledTaskTrigger -AtLogOn -User $currentSID
-# $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File $tempFilePath"
-# Register-ScheduledTask -TaskName ABC  -Action $action -Trigger $trigger -User $currentSID -Password "Rjangra#2021" -TaskPath "\"
+Register-ScheduledTask -TaskName ABC  -Action $action -Trigger $trigger -User $currentSID -Password "Rjangra#2021" -TaskPath "\"
 Stop-Transcript
