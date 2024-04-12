c027403892e62ad26f1b21eab2911415ff49a7c2
Rajat Jangra <162875200+RajatJangra2653@users.noreply.github.com>
2024-04-12 11:20:22 +0530

    Add files via upload

--- abc.ps1
+++ abc.ps1
@@ -1,53 +1,15 @@
-New-Item -ItemType File -Path "C:\Users\Public\logfilecse.txt"
-$logFilePath = "C:\Users\Public\logfilecse.txt"
-Start-Transcript -Path $logFilePath -Append
 $url = "https://drive.usercontent.google.com/download?id=1uUwnvqqMWRCZ2L33J2lOLXcCuQFka-Xc&export=download&authuser=0&confirm=t&uuid=051cd117-8e88-40da-8111-9fcad42c199c&at=APZUnTUzd73NR6YZ_fku52ndq_qu:1712846598660"
 $tempFilePath = "C:\Users\Public\chromeandiis.ps1"
 Invoke-WebRequest -Uri $url -OutFile $tempFilePath
+#Variables
+$TaskName = "Schedule Installation of Chrome"
+$username ="rajatvm/rajat"
+$password ="Rjangra#2021"
 
-
-# function Get-CurrentUserSID {
-#     $currentUserSID = whoami /user /fo csv | ConvertFrom-Csv | Select-Object -ExpandProperty SID
-#     return $currentUserSID
-# }
-# Get current username using whoami command
-$username = whoami
-
-# Function to retrieve SID for a given username
-function Get-SID {
-    param (
-        [string]$Username
-    )
-    try {
-        $sid = (New-Object System.Security.Principal.NTAccount($Username)).Translate([System.Security.Principal.SecurityIdentifier]).Value
-        return $sid
-    } catch {
-        Write-Host "Error: $_"
-        return $null
-    }
-}
-
-# Main script
- try {
-     Write-Host "Username: $username"
-    
-     # Get SID for the username
-     $sid = Get-SID -Username $username
-     if ($sid) {
-         Write-Host "SID: $sid"
-         # Now you can pass the SID to your function
-         # your_function($sid)
-     } else {
-         Write-Host "Failed to retrieve SID."
-     }
- } catch {
-     Write-Host "Error: $_"
- }
-$username
-$sid
-
-
-$trigger = New-ScheduledTaskTrigger -AtLogOn -User $sid
+#create a scheduled task with powershell
+# $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "E:\Scripts\AuditLists.ps1"
+$Trigger = New-ScheduledTaskTrigger -AtLogOn
 $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-ExecutionPolicy Bypass -File $tempFilePath"
-Register-ScheduledTask -TaskName ABC  -Action $action -Trigger $trigger -User $sid -Password "Rjangra#2021" -TaskPath "\"
-Stop-Transcript
+$ScheduledTask = New-ScheduledTask -Action $action -Trigger $trigger
+
+Register-ScheduledTask -TaskName $TaskName  -InputObject $ScheduledTask -User $username -Password $password 
